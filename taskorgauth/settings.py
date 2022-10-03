afe9152f81f8b3e467dfec244487455681855130
Leal31 <lealemanuel31@protonmail.com>
2022-10-03 07:14:17 -0500

    databaseConfig

--- taskorgauth/settings.py
+++ taskorgauth/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +75,16 @@ WSGI_APPLICATION = 'taskorgauth.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": os.getenv("PGDATABASE"),
+        "USER": os.getenv("PGUSER"),
+        "HOST": os.getenv("PGHOST"),
+        "PASSWORD": os.getenv("PGPASSWORD"),
+        "PORT": int(os.getenv("PGPORT"))
+    }
 }
 
 # Password validation
